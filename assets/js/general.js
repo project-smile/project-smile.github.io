42b3530433769a042f74d5815945b7744103f642
Sander de Groot <sanderg@infosupport.com>
2016-12-19 22:19:46 +0100

    Moved ga code to general.js

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,4 +1,26 @@
-// setup event tracking
+// google analytics code
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+if (window.location.hostname.lastIndexOf('projectsmile.nl') > -1) {
+    // prd profile
+    ga('create', 'UA-85533742-1', 'auto');
+} else {
+    //dev profile
+    ga('create', 'UA-85533742-2', 'auto');
+}
+ga('send', 'pageview');
+
+
 document.addEventListener('DOMContentLoaded', function () {
     ga('set', 'dimension1', 'unknown'); // dimension1 = card
 
